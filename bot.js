a34015f9c5b30035f88881d53125fa05680a896a
Shishir Sharma <shishirsharma.in@gmail.com>
2017-09-07 11:37:50 +0530

    Add add proper config for redis

--- bot.js
+++ bot.js
@@ -82,7 +82,7 @@ if (process.env.MONGO_URI) {
     var mongoStorage = require('botkit-storage-mongo')({mongoUri: process.env.MONGO_URI});
     bot_options.storage = mongoStorage;
 } else if (process.env.REDIS_URL) {
-    var redis_config = process.env.REDIS_URL;
+    var redis_config = {url: process.env.REDIS_URL};
     var redisStorage = require('botkit-storage-redis')(redis_config);
     bot_options.storage = redisStorage;
 } else {
