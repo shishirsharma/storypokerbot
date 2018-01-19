ffeb1398a54d55a528b67997d303d15effb9b4f2
Shishir Sharma <shishirsharma.in@gmail.com>
2018-01-19 12:42:27 +0530

    Repoint should have same points

--- skills/interactve_messages.js
+++ skills/interactve_messages.js
@@ -98,151 +98,151 @@ module.exports = function(controller) {
                 var value = {};
                 var reply =  trigger.original_message;
                 reply.attachments =  [
-                        {
-                            "fallback": "Pre-filled because you have actions in your attachment.",
-                            "color": "#bdc3c7",
-                            "mrkdwn_in": [
-                                "text",
-                                "pretext",
-                                "fields"
-                            ],
-                            "callback_id": "select_point_action",
-                            "attachment_type": "default",
-                            "actions": [
-                                {
-                                    "name": "1",
-                                    "text": "1",
-                                    "type": "button",
-                                    "style": "default",
-                                    "value": "1"
-                                },
-                                {
-                                    "name": "2",
-                                    "text": "2",
-                                    "type": "button",
-                                    "style": "default",
-                                    "value": "2"
-                                },
-                                {
-                                    "name": "3",
-                                    "text": "3",
-                                    "type": "button",
-                                    "style": "default",
-                                    "value": "3"
-                                },
-                                {
-                                    "name": "5",
-                                    "text": "5",
-                                    "type": "button",
-                                    "style": "default",
-                                    "value": "5"
-                                },
-                                {
-                                    "name": "8",
-                                    "text": "8",
-                                    "type": "button",
-                                    "style": "default",
-                                    "value": "8"
-                                }
-                            ]
-                        },
-                        {
-                            "fallback": "Pre-filled because you have actions in your attachment.",
-                            "color": "#bdc3c7",
-                            "mrkdwn_in": [
-                                "text",
-                                "pretext",
-                                "fields"
-                            ],
-                            "callback_id": "select_point_action",
-                            "attachment_type": "default",
-                            "actions": [
-                                {
-                                    "name": "Select point",
-                                    "text": "Select point",
-                                    "type": "select",
-                                    "value": "Select point",
-                                    "data_source": "static",
-                                    "options": [
-                                        {
-                                            "text": "1",
-                                            "value": "1"
-                                        },
-                                        {
-                                            "text": "2",
-                                            "value": "2"
-                                        },
-                                        {
-                                            "text": "3",
-                                            "value": "3"
-                                        },
-                                        {
-                                            "text": "4",
-                                            "value": "4"
-                                        },
-                                        {
-                                            "text": "5",
-                                            "value": "5"
-                                        },
-                                        {
-                                            "text": "6",
-                                            "value": "6"
-                                        },
-                                        {
-                                            "text": "7",
-                                            "value": "7"
-                                        },
-                                        {
-                                            "text": "8",
-                                            "value": "8"
-                                        },
-                                        {
-                                            "text": "9",
-                                            "value": "9"
-                                        },
-                                        {
-                                            "text": "10",
-                                            "value": "10"
-                                        }
-                                    ]
-                                }
-                            ]
-                        },
-                        {
-                            "fallback": "Pre-filled because you have actions in your attachment.",
-                            "color": "#bdc3c7",
-                            "mrkdwn_in": [
-                                "text",
-                                "pretext",
-                                "fields"
-                            ],
-                            "callback_id": "select_poker_action",
-                            "attachment_type": "default",
-                            "actions": [
-                                {
-                                    "name": "Reveal",
-                                    "text": "Reveal",
-                                    "type": "button",
-                                    "style": "default",
-                                    "value": `${JSON.stringify(value)}`
-                                },
-                                {
-                                    "name": "Repoint",
-                                    "text": "Repoint",
-                                    "type": "button",
-                                    "style": "default",
-                                    "value": `${JSON.stringify(value)}`
-                                },
-                                {
-                                    "name": "Dismiss",
-                                    "text": "Dismiss",
-                                    "type": "button",
-                                    "style": "default",
-                                    "value": "Dismiss"
-                                }
-                            ]
-                        }
-                    ]
+                    {
+                        "fallback": "Pre-filled because you have actions in your attachment.",
+                        "color": "#bdc3c7",
+                        "mrkdwn_in": [
+                            "text",
+                            "pretext",
+                            "fields"
+                        ],
+                        "callback_id": "select_point_action",
+                        "attachment_type": "default",
+                        "actions": [
+                            {
+                                "name": "1",
+                                "text": "1",
+                                "type": "button",
+                                "style": "default",
+                                "value": "1"
+                            },
+                            {
+                                "name": "2",
+                                "text": "2",
+                                "type": "button",
+                                "style": "default",
+                                "value": "2"
+                            },
+                            {
+                                "name": "3",
+                                "text": "3",
+                                "type": "button",
+                                "style": "default",
+                                "value": "3"
+                            },
+                            {
+                                "name": "5",
+                                "text": "5",
+                                "type": "button",
+                                "style": "default",
+                                "value": "5"
+                            },
+                            {
+                                "name": "8",
+                                "text": "8",
+                                "type": "button",
+                                "style": "default",
+                                "value": "8"
+                            }
+                        ]
+                    },
+                    {
+                        "fallback": "Pre-filled because you have actions in your attachment.",
+                        "color": "#bdc3c7",
+                        "mrkdwn_in": [
+                            "text",
+                            "pretext",
+                            "fields"
+                        ],
+                        "callback_id": "select_point_action",
+                        "attachment_type": "default",
+                        "actions": [
+                            {
+                                "name": "Select point",
+                                "text": "Select point",
+                                "type": "select",
+                                "value": "Select point",
+                                "data_source": "static",
+                                "options": [
+                                    {
+                                        "text": "0",
+                                        "value": "0"
+                                    },
+                                    {
+                                        "text": "1",
+                                        "value": "1"
+                                    },
+                                    {
+                                        "text": "2",
+                                        "value": "2"
+                                    },
+                                    {
+                                        "text": "3",
+                                        "value": "3"
+                                    },
+                                    {
+                                        "text": "5",
+                                        "value": "5"
+                                    },
+                                    {
+                                        "text": "8",
+                                        "value": "8"
+                                    },
+                                    {
+                                        "text": "13",
+                                        "value": "13"
+                                    },
+                                    {
+                                        "text": "21",
+                                        "value": "21"
+                                    },
+                                    {
+                                        "text": "34",
+                                        "value": "34"
+                                    },
+                                    {
+                                        "text": "55",
+                                        "value": "55"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "fallback": "Pre-filled because you have actions in your attachment.",
+                        "color": "#bdc3c7",
+                        "mrkdwn_in": [
+                            "text",
+                            "pretext",
+                            "fields"
+                        ],
+                        "callback_id": "select_poker_action",
+                        "attachment_type": "default",
+                        "actions": [
+                            {
+                                "name": "Reveal",
+                                "text": "Reveal",
+                                "type": "button",
+                                "style": "default",
+                                "value": `${JSON.stringify(value)}`
+                            },
+                            {
+                                "name": "Repoint",
+                                "text": "Repoint",
+                                "type": "button",
+                                "style": "default",
+                                "value": `${JSON.stringify(value)}`
+                            },
+                            {
+                                "name": "Dismiss",
+                                "text": "Dismiss",
+                                "type": "button",
+                                "style": "default",
+                                "value": "Dismiss"
+                            }
+                        ]
+                    }
+                ]
 
 
             }else if(trigger.actions[0].name.match(/^Dismiss$/)){
