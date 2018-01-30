154035eae83b1dd8dce3f069a67961584177baba
Shishir Sharma <shishirsharma.in@gmail.com>
2018-01-31 02:00:47 +0530

    Enable Advanced Botkit Studio Analytics

--- bot.js
+++ bot.js
@@ -94,6 +94,8 @@ var controller = Botkit.slackbot(bot_options);
 
 controller.startTicking();
 
+require('botkit-studio-metrics')(controller);
+
 // Set up an Express-powered webserver to expose oauth and webhook endpoints
 var webserver = require(__dirname + '/components/express_webserver.js')(controller);
 
