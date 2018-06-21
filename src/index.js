1dd86551167b20f2ef3490981fa1b0373be0a548
Shishir Sharma <shishirsharma.in@gmail.com>
2018-06-21 16:42:34 +0530

    Added client list

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@
 //     <link rel="stylesheet" href="/landing-styles.css" />
 import styles from './styles/app.scss';
 import Prism from 'prismjs';
+
+import './slick-slide';
---
+++ src/slick-slide.js
@@ -0,0 +1,25 @@
+import $ from 'jquery';
+import 'slick-carousel';
+
+$(document).ready(function(){
+		$('.customer-logos').slick({
+				slidesToShow: 5,
+				slidesToScroll: 1,
+				autoplay: true,
+				autoplaySpeed: 1000,
+				arrows: false,
+				dots: false,
+				pauseOnHover: false,
+				responsive: [{
+					  breakpoint: 768,
+					  settings: {
+						    slidesToShow: 3
+					  }
+				}, {
+					  breakpoint: 520,
+					  settings: {
+						    slidesToShow: 2
+					  }
+				}]
+		});
+});
