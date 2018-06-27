2c4ee35b0c90958179ada6c4cfc7a473eab5c94b
Shishir Sharma <shishirsharma.in@gmail.com>
2018-06-27 16:58:08 +0530

    Added more info in mixpenel users

--- components/plugin_mixpanel.js
+++ components/plugin_mixpanel.js
@@ -1,6 +1,6 @@
-var request = require('request');
-var md5 = require('md5');
-var botkit_mixpanel_metrics = function(controller, options) {
+let request = require('request');
+let md5 = require('md5');
+let botkit_mixpanel_metrics = function(controller, options) {
 
   if (!options) {
     options = {
@@ -11,73 +11,74 @@ var botkit_mixpanel_metrics = function(controller, options) {
 
   // Setup mixpanel
   // grab the Mixpanel factory
-  var Mixpanel = require('mixpanel');
+  let Mixpanel = require('mixpanel');
 
   // create an instance of the mixpanel client
   // initialize mixpanel client configured to communicate over https
-  var mixpanel = Mixpanel.init(options.mixpanel_api_key, {
+  let mixpanel = Mixpanel.init(options.mixpanel_api_key, {
     protocol: 'https'
   });
 
 
-  var bot_metrics_url_base = controller.config.studio_command_uri || 'https://api.botkit.ai';
+  let bot_metrics_url_base = controller.config.studio_command_uri || 'https://api.botkit.ai';
 
-  var debug = options.debug;
+  let debug = options.debug;
 
-  var BotMetrics = {
+  let BotMetrics = {
 
-    callAPI: function(url, payload, cb) {
+    // callAPI: function(url, payload, cb) {
 
-      var options = {
-        rejectUnauthorized: false,
-        url: bot_metrics_url_base + url + '?access_token=' + controller.config.studio_token,
-        method: 'POST',
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(payload),
-      };
+    //   let options = {
+    //     rejectUnauthorized: false,
+    //     url: bot_metrics_url_base + url + '?access_token=' + controller.config.studio_token,
+    //     method: 'POST',
+    //     headers: {
+    //       "content-type": "application/json",
+    //     },
+    //     body: JSON.stringify(payload),
+    //   };
 
-      if (controller.config.studio_token) {
-
-
-        request(options, function(err, res, body) {
-
-          if (err) {
-            return cb(err);
-          }
-
-          if (res.statusCode === 202) {
-            return cb(null, {});
-          }
+    //   if (controller.config.studio_token) {
 
 
+    //     request(options, function(err, res, body) {
 
-          try {
-            json = JSON.parse(body);
-          } catch (e) {
-            return cb(e);
-          }
-          if (json.error) {
-            if (res.statusCode === 401) {
-              console.error(json.error);
-            }
-            return cb(json.error);
-          } else if (json.errors) {
-            if (res.statusCode === 401) {
-              console.error(json.errors[0]);
-            }
-            return cb(json.errors[0]);
-          } else {
-            return cb(null, json.data);
-          }
+    //       if (err) {
+    //         return cb(err);
+    //       }
 
-        });
-      }
-    },
+    //       if (res.statusCode === 202) {
+    //         return cb(null, {});
+    //       }
+
+
+
+    //       try {
+    //         json = JSON.parse(body);
+    //       } catch (e) {
+    //         return cb(e);
+    //       }
+    //       if (json.error) {
+    //         if (res.statusCode === 401) {
+    //           console.error(json.error);
+    //         }
+    //         return cb(json.error);
+    //       } else if (json.errors) {
+    //         if (res.statusCode === 401) {
+    //           console.error(json.errors[0]);
+    //         }
+    //         return cb(json.errors[0]);
+    //       } else {
+    //         return cb(null, json.data);
+    //       }
+
+    //     });
+    //   }
+    // }
+    // ,
     receiveEvent: function(bot, message) {
 
-      var that = this;
+      let that = this;
 
       // {
       //   distinct_id: 'some unique client id',
@@ -85,7 +86,7 @@ var botkit_mixpanel_metrics = function(controller, options) {
       //   properties: 'as',
       //   you: 'want'
       // }
-      var payload = {
+      let payload = {
         distinct_id: message.user,
         instance_uid: bot.identity.id,
         provider: 'generic',
@@ -132,7 +133,7 @@ var botkit_mixpanel_metrics = function(controller, options) {
         //     if (debug) console.error('METRICS ERROR', err);
         //   } else {
         //     if (res.messages) {
-        //       for (var m = 0; m < res.messages.length; m++) {
+        //       for (let m = 0; m < res.messages.length; m++) {
         //         if (res.messages[m].code == 'bot_user_not_found') {
         //           that.user(bot, message);
         //         }
@@ -146,152 +147,198 @@ var botkit_mixpanel_metrics = function(controller, options) {
         // });
       }
     },
-    sendEvent: function(bot, message) {
+    // sendEvent: function(bot, message) {
 
-      var that = this;
+    //   let that = this;
 
-      var payload = {
-        instance_uid: bot.identity.id, // slack team id
-        provider: 'generic',
-        event: {
-          text: message.text,
-          event_type: 'message',
-          is_for_bot: false,
-          is_from_bot: true,
-          is_im: false,
-          timestamp: new Date(),
-          user_id: message.to,
-        }
-      }
+    //   let payload = {
+    //     instance_uid: bot.identity.id, // slack team id
+    //     provider: 'generic',
+    //     event: {
+    //       text: message.text,
+    //       event_type: 'message',
+    //       is_for_bot: false,
+    //       is_from_bot: true,
+    //       is_im: false,
+    //       timestamp: new Date(),
+    //       user_id: message.to,
+    //     }
+    //   }
 
-      if (debug) {
-        console.log('Send event', payload);
-      }
+    //   if (debug) {
+    //     console.log('Send event', payload);
+    //   }
 
-      that.callAPI('/api/v2/stats/events', payload, function(err, res) {
-        if (err) {
-          if (debug) console.error('METRICS ERROR', err);
-        } else {
-          if (res.messages) {
-            for (var m = 0; m < res.messages.length; m++) {
-              if (res.messages[m].code == 'bot_user_not_found') {
-                that.user(bot, {
-                  user: message.to,
-                  channel: message.channel
-                });
-              }
+    //   that.callAPI('/api/v2/stats/events', payload, function(err, res) {
+    //     if (err) {
+    //       if (debug) console.error('METRICS ERROR', err);
+    //     } else {
+    //       if (res.messages) {
+    //         for (let m = 0; m < res.messages.length; m++) {
+    //           if (res.messages[m].code == 'bot_user_not_found') {
+    //             that.user(bot, {
+    //               user: message.to,
+    //               channel: message.channel
+    //             });
+    //           }
 
-              if (res.messages[m].code == 'bot_instance_not_found') {
-                that.instance(bot, message);
-              }
-            }
-          }
-        }
-      });
-    },
+    //           if (res.messages[m].code == 'bot_instance_not_found') {
+    //             that.instance(bot, message);
+    //           }
+    //         }
+    //       }
+    //     }
+    //   });
+    // },
     user: function(bot, message) {
 
-      var that = this;
-      if (bot.getMessageUser) {
-        bot.getMessageUser(message).then(function(profile) {
-          var payload = {
-            $first_name: profile.first_name || 'unknown',
-            $last_name: profile.last_name || 'unknown',
-            $email: profile.email || 'unknown',
-            $phone: profile.phone || 'unknown',
-            title: profile.title || 'unknown',
-            // $created: (new Date()).toISOString(),
-            instance_uid: bot.identity.id,
-            user_id: profile.id,
-            profile_pic_url: null,
-            nickname: profile.username || 'unknown',
-            full_name: profile.full_name || 'unknown',
-            gender: profile.gender || 'unknown',
-            timezone: profile.timezone || "America/Los_Angeles",
-            timezone_offset: profile.timezone_offset || null,
-          }
+      let that = this;
 
-          if (debug) {
-            console.log('Send user', payload);
-          }
 
-          // create or update a user in Mixpanel Engage without altering $last_seen
-          // - pass option $ignore_time: true to prevent the $last_seen property from being updated
-          mixpanel.people.set(message.user, payload, {
-            $ignore_time: true
-          });
-
-          // that.callAPI('/api/v2/stats/bot_users', payload, function(err, res) {
-          //   if (err) {
-          //     if (debug) console.error('Error in bot user metrics API: ', err);
-          //   }
-          // });
-        }).catch(function(err) {
-          if (err) {
-            if (debug) console.error('Error in bot user metrics API: ', err);
-          }
-        });
-      }
-    },
-    instance: function(bot, message) {
-      var that = this;
-      var payload = {
-        uid: bot.identity.id,
-        attributes: {
-          name: bot.identity.name,
-          id: bot.identity.id,
-        }
-      }
-
-      if (bot.getInstanceInfo) {
-        bot.getInstanceInfo().then(function(instance) {
-
-          payload.attributes.name = instance.identity.name;
-          payload.attributes.id = instance.identity.id;
-          payload.attributes.team_name = instance.team.name;
-          payload.attributes.team_url = instance.team.url;
-          payload.attributes.team_id = instance.team.id;
-
-          if (debug) {
-            console.log('Send instance', payload);
-          }
-
-          that.callAPI('/api/v2/stats/instances', payload, function(err, res) {
-            if (err) {
-              if (debug) console.error('Error in bot instance metrics API: ', err);
-            }
-          });
-        }).catch(function(err) {
-          if (err) {
-            if (debug) console.error('Error in bot instance metrics API: ', err);
-          }
-        });
-      } else {
+      if (!bot.getMessageUserMixpanel) {
         if (debug) {
-          console.log('Send instance', payload);
+          console.log('Setting bot.getMessageUserMixpanel');
         }
 
-        that.callAPI('/api/v2/stats/instances', payload, function(err, res) {
-          if (err) {
-            if (debug) console.error('Error in bot instance metrics API: ', err);
-          }
-        });
+        bot.getMessageUserMixpanel = function(message, cb) {
+          return new Promise(function(resolve, reject) {
+            bot.api.users.info({user: message.user}, function(err, identity) {
+              if (err) {
+                if (cb) {
+                  cb(err);
+                }
+                return reject(err);
+              }
+              // normalize this into what botkit wants to see
+              let profile = {
+                id: message.user,
+                username: identity.user.profile.display_name,
+                first_name: identity.user.profile.first_name,
+                last_name: identity.user.profile.last_name,
+                full_name: identity.user.profile.real_name,
+                email: identity.user.profile.email, // may be blank
+                gender: null, // no source for this info
+                timezone_offset: (identity.user.tz_offset / (60 * 60)), // convert from seconds to hours
+                timezone: identity.user.tz
+              };
+              if (cb) {
+                cb(null, profile, identity);
+              }
+              resolve({profile, identity});
+            });
+          });
+
+        };
       }
+
+      bot.getMessageUserMixpanel(message).then(function(options) {
+        let profile = options.profile;
+        let identity = options.identity;
+        let payload = {
+          $first_name: profile.first_name || 'unknown',
+          $last_name: profile.last_name || 'unknown',
+          $email: profile.email || 'unknown',
+          $phone: profile.phone || 'unknown',
+          title: profile.title || 'unknown',
+          // $created: (new Date()).toISOString(),
+          instance_uid: bot.identity.id,
+          user_id: profile.id,
+          team_id: identity.user.team_id,
+          is_admin: identity.user.is_admin,
+          is_owner: identity.user.is_owner,
+          is_primary_owner: identity.user.is_primary_owner,
+          is_restricted: identity.user.is_restricted,
+          is_ultra_restricted: identity.user.is_ultra_restricted,
+          is_bot: identity.user.is_bot,
+          profile_pic_url: identity.user.profile.image_512,
+          nickname: profile.username || 'unknown',
+          full_name: profile.full_name || 'unknown',
+          gender: profile.gender || 'unknown',
+          timezone: profile.timezone || "America/Los_Angeles",
+          timezone_offset: profile.timezone_offset || null,
+        }
+
+        if (debug) {
+          console.log('Send user', payload);
+        }
+
+        // create or update a user in Mixpanel Engage without altering $last_seen
+        // - pass option $ignore_time: true to prevent the $last_seen property from being updated
+        mixpanel.people.set(message.user, payload, {
+          $ignore_time: true
+        });
+
+        // that.callAPI('/api/v2/stats/bot_users', payload, function(err, res) {
+        //   if (err) {
+        //     if (debug) console.error('Error in bot user metrics API: ', err);
+        //   }
+        // });
+      }).catch(function(err) {
+        if (err) {
+          if (debug) console.error('Error in bot user metrics API: ', err);
+        }
+      });
+
     }
+    // instance: function(bot, message) {
+    //   let that = this;
+    //   let payload = {
+    //     uid: bot.identity.id,
+    //     attributes: {
+    //       name: bot.identity.name,
+    //       id: bot.identity.id,
+    //     }
+    //   }
+
+    //   if (bot.getInstanceInfo) {
+    //     bot.getInstanceInfo().then(function(instance) {
+
+    //       payload.attributes.name = instance.identity.name;
+    //       payload.attributes.id = instance.identity.id;
+    //       payload.attributes.team_name = instance.team.name;
+    //       payload.attributes.team_url = instance.team.url;
+    //       payload.attributes.team_id = instance.team.id;
+
+    //       if (debug) {
+    //         console.log('Send instance', payload);
+    //       }
+
+    //       that.callAPI('/api/v2/stats/instances', payload, function(err, res) {
+    //         if (err) {
+    //           if (debug) console.error('Error in bot instance metrics API: ', err);
+    //         }
+    //       });
+    //     }).catch(function(err) {
+    //       if (err) {
+    //         if (debug) console.error('Error in bot instance metrics API: ', err);
+    //       }
+    //     });
+    //   } else {
+    //     if (debug) {
+    //       console.log('Send instance', payload);
+    //     }
+
+    //     that.callAPI('/api/v2/stats/instances', payload, function(err, res) {
+    //       if (err) {
+    //         if (debug) console.error('Error in bot instance metrics API: ', err);
+    //       }
+    //     });
+    //   }
+    // }
 
   }
 
-  controller.middleware.heard.use(function(bot, message, next) {
-    // BotMetrics.receiveEvent(bot, message);
-    // if (options.always_update) {
-    //   BotMetrics.user(bot, message);
-    //   BotMetrics.instance(bot, message);
-    // }
-    next();
-  });
+  // controller.middleware.heard.use(function(bot, message, next) {
+  //   BotMetrics.receiveEvent(bot, message);
+  //   if (options.always_update) {
+  //     BotMetrics.user(bot, message);
+  //     BotMetrics.instance(bot, message);
+  //   }
+  //   next();
+  // });
 
   controller.middleware.triggered.use(function(bot, message, next) {
-    var relevant_events = ['message_received', 'direct_message', 'direct_mention', 'mention', 'ambient', 'facebook_postback', 'interactive_message_callback', 'slash_command', 'invoke'];
+    let relevant_events = ['message_received', 'direct_message', 'direct_mention', 'mention', 'ambient', 'facebook_postback', 'interactive_message_callback', 'slash_command', 'invoke'];
     if (message && message.type && relevant_events.indexOf(message.type) != -1) {
       BotMetrics.receiveEvent(bot, message);
       if (options.always_update) {
@@ -302,13 +349,13 @@ var botkit_mixpanel_metrics = function(controller, options) {
     next();
   });
 
-  controller.middleware.send.use(function(bot, message, next) {
-    // BotMetrics.sendEvent(bot, message);
-    // if (options.always_update) {
-    //   BotMetrics.instance(bot, message);
-    // }
-    next();
-  });
+  // controller.middleware.send.use(function(bot, message, next) {
+  //   BotMetrics.sendEvent(bot, message);
+  //   if (options.always_update) {
+  //     BotMetrics.instance(bot, message);
+  //   }
+  //   next();
+  // });
 
   return BotMetrics;
 
@@ -321,17 +368,17 @@ module.exports = function(controller) {
   // Sign up for a free key here: https://www.dashbot.io/ to see your bot analytics in real time.
   if (process.env.MIXPANEL_API_KEY) {
     // Setup mixpanel
-    botkit_mixpanel_metrics(controller, {mixpanel_api_key: process.env.MIXPANEL_API_KEY, debug: false, always_update: true});
+    botkit_mixpanel_metrics(controller, {mixpanel_api_key: process.env.MIXPANEL_API_KEY, debug: true, always_update: true});
     // // grab the Mixpanel factory
-    // var Mixpanel = require('mixpanel');
+    // let Mixpanel = require('mixpanel');
 
     // // create an instance of the mixpanel client
     // // initialize mixpanel client configured to communicate over https
-    // var mixpanel = Mixpanel.init(process.env.MIXPANEL_API_KEY, {
+    // let mixpanel = Mixpanel.init(process.env.MIXPANEL_API_KEY, {
     //     protocol: 'https'
     // });
 
-    // // var dashbot = require('dashbot')(process.env.DASHBOT_API_KEY).slack;
+    // // let dashbot = require('dashbot')(process.env.DASHBOT_API_KEY).slack;
     // // controller.middleware.receive.use(dashbot.receive);
     // // controller.middleware.send.use(dashbot.send);
     // // controller.log.info('Thanks for using Dashbot. Visit https://www.dashbot.io/ to see your bot analytics in real time.');
